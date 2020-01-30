c643e9012d3c9489812a93884a24ff3e465d2957
spenny180 <59981850+spenny180@users.noreply.github.com>
2020-01-30 16:33:09 -0600

    Update Program.cs

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -10,28 +10,40 @@ namespace WeatherCalculator2
 
             int temperature;
             int windSpeed;
+            int RelativeHumidity;
             double windChill;
+            double heatIndex;
             string input = "";
-
-            // Get the temperature from the user
             Console.WriteLine("Enter the temperature");
 
             input = Console.ReadLine();
 
             temperature = int.Parse(input);
 
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+            if (temperature > 49.9)
+            {
+                Console.WriteLine("Enter the Humidity");
+                input = Console.ReadLine();
+                RelativeHumidity = int.Parse(input);
+                heatIndex = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + RelativeHumidity * 0.094);
+                Console.WriteLine("The Heat Index is " + heatIndex);
 
-            input = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Enter the wind speed");
 
-            windSpeed = int.Parse(input);
+                input = Console.ReadLine();
 
-            // Calculate the wind chill
+                windSpeed = int.Parse(input);
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
 
-            Console.WriteLine("The wind chill is " + windChill);
+                Console.WriteLine("The wind chill is " + windChill);
+
+            }
+      
+
         }
     }
 }
